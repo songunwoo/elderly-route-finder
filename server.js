8027518a8771c441ca11425402a2309d11b66ab1
Gws-kevin <100399046+songunwoo@users.noreply.github.com>
2026-08-21 17:57:00 +0900

    Update server.js

--- server.js
+++ server.js
@@ -58,9 +58,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
 app.post('/api/routes', async (req, res) => {
   try {
     const { origin, destination, transportType, profile } = req.body;
